26753ede9acbbd8545a1dd164f74b1c8ca333e84
AliouacheneAnis <eaalioua01@monccnb.ca>
2022-03-21 00:25:15 -0300

    Version Final

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+/*
+  Titre      : Controle LED avec clavier matriciel 
+  Auteur     : Anis Aliouachene
+  Date       : 21/03/2022
+  Description: Le programme permets de controler 4 led a l'aide d'un clavier matriciel ou les chiffres 0 a 9 designe l'intensite  et les boutton 
+  A,B,C,D designe les LED et on utilise les broche PWM pour allumer les LED 
+  Version    : 0.0.1
+*/
+
+
 #include <Arduino.h>
 #include <WIFIConnector_MKR1010.h>
 #include "MQTTConnector.h"
